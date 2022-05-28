ab6a24bc1fb76af37c5f3ede0238f76944a898c1
Abongwa Bonalais Amahnui <abongwabonalais@gmail.com>
2022-05-28 19:24:36 +0100

    innitial commits

---
+++ texts.py
@@ -0,0 +1,8 @@
+print("""WELCOME TO THE ADVENTURE GAME!
+    Let's start the action! ☆-🎬-☆
+     
+    Lily wakes up in her bedroom in the middle of the night. She heard a loud BAN outside the house.
+    Now she has two choices she can either stay in the room or check what the sound might be about.
+     
+    Type your choice: Stay or Evaluate?
+""")
